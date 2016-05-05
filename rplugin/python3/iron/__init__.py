3af178093edd65e799533674dee3606d43f4ffa3
Henry Kupty <hkupty@gmail.com>
2016-05-04 22:28:07 -0300

    Restructure main logic

--- rplugin/python3/iron/__init__.py
+++ rplugin/python3/iron/__init__.py
@@ -3,8 +3,11 @@
 
 `iron` is a plugin that allows better interactions with interactive repls
 using neovim's job-control and terminal.
+
+Currently it keeps track of a single repl instance per filetype.
 """
 import neovim
+from copy import deepcopy
 from iron.repls import available_repls
 
 
@@ -13,30 +16,24 @@ class Iron(object):
 
     def __init__(self, nvim):
         self.__nvim = nvim
-        self.__repls = {}
-        self.__current = -1
+        self.__repl = {}
 
     def get_repl_template(self, ft):
-        ft_repl = "iron_{}_repl".format(ft)
+        repls = list(filter(lambda k: ft in k['languages'],
+            available_repls))
+        # TODO Wiser choosing
+        return len(repls) and repls[0] or {}
 
-        if ft_repl in self.__nvim.vars:
-            return self.__nvim.vars[ft_repl]
-        else:
-            repls = list(filter(lambda k: ft in k['languages'],
-                available_repls))
-            # TODO Wiser choosing
-            return len(repls) and repls[0]['command'] or ""
-
-    @neovim.function("IronOpenRepl")
-    def open_repl_for(self, args):
+    def open_repl_for(self, ft):
         self.__nvim.command('spl | wincmd j | enew')
-        repl_id = self.__nvim.call('termopen', args[0])
+        repl_id = self.__nvim.call('termopen', self.__repl[ft]['command'])
+
+        # TODO Make optional nvimux integration detached
         self.__nvim.current.buffer.vars['nvimux_buf_orientation'] = (
             "botright split"
         )
 
-        self.__repls[repl_id] = list(filter(lambda k: args[0] == k['command'],
-            available_repls))[0]
+        self.__repls[ft]['repl_id'] = repl_id
 
         self.__current = repl_id
         return repl_id
@@ -44,10 +41,15 @@ class Iron(object):
     @neovim.command("IronRepl")
     def get_repl(self):
         ft = self.__nvim.current.buffer.options["ft"]
-        repl_type = self.get_repl_template(ft)
 
-        if repl_type == "":
+        if ft in self.__repl:
+            return
+
+        repl_type = self.__repl[ft] = self.get_repl_template(ft)
+
+        if not repl_type:
             self.__nvim.command("echoerr 'No repl found for {}'".format(ft))
+
         else:
             repl_id = self.open_repl_for([repl_type])
             repl_buffer_id = self.__nvim.current.buffer.number
@@ -55,6 +57,17 @@ class Iron(object):
 
     @neovim.function("IronSendToRepl")
     def send_to_repl(self, args):
+        ft = (
+            args[1]
+            if len(args) > 1 else
+            self.__nvim.current.buffer.options['ft']
+        )
+
+        repl = self.__repl[ft] if ft in self.__repl else None
+
+        if not repl:
+            return
+
         if args[0] == 'line':
             self.__nvim.command("""normal! '[V']"sy""")
         else:
@@ -62,13 +75,11 @@ class Iron(object):
 
         data = self.__nvim.funcs.getreg('s')
 
-        multiline = 'multinine' in self.__repls[self.__current]
+        multiline = 'multinine' in repl
 
-        if multiline and\
-                any(map(lambda k: not k or k.isspace(), data.split('\n'))):
-            (pre, post) = self.__repls[self.__current]['multiline']
+        if multiline and any(map(
+                lambda k: not k or k.isspace(), data.split('\n'))):
+            (pre, post) = repl['multiline']
             data = "{}\n{}\n{}".format(pre, data, post)
 
-        data += "\n"
-
-        self.__nvim.call('jobsend', self.__current, data)
+        self.__nvim.call('jobsend', repl['repl_id'], data)
