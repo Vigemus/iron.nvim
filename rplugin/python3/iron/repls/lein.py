3e3be8ba542f01f7caf68debfa0b13773ffa38e5
Henry John Kupty <henry.kupty@nubank.com.br>
2016-05-11 18:11:40 -0300

    lein: Add some functions to clojure repl

--- rplugin/python3/iron/repls/lein.py
+++ rplugin/python3/iron/repls/lein.py
@@ -1,15 +1,33 @@
 # encoding:utf-8
 """Leiningen repl definition for iron.nvim. """
 
-def send_object_to_repl(nvim):
+def lein_require(nvim):
     nvim.command("""normal! "sya]""")
     data = "(require '{})\n".format(nvim.funcs.getreg('s'))
+    return nvim.call('jobsend', repl['repl_id'], data)
+
+def lein_import(nvim):
+    nvim.command("""normal! "syi(""")
+    data = "(import '[{}])\n".format(nvim.funcs.getreg('s'))
+    return nvim.call('jobsend', repl['repl_id'], data)
+
+def lein_require_file(nvim):
+    nvim.command("""normal! ggf w"sy$""")
+    data = "(require '[{}] :reload)\n".format(nvim.funcs.getreg('s'))
+    return nvim.call('jobsend', repl['repl_id'], data)
+
+def lein_send(nvim):
+    nvim.command("""normal! "sya(""")
+    data = "(import '[{}])\n".format(nvim.funcs.getreg('s'))
     return nvim.call('jobsend', repl['repl_id'], data)
 
 repl = {
     'command': 'lein repl',
     'language': 'clojure',
     'mappings': [
-        ('so', send_object_to_repl),
+        ('so', lein_require),
+        ('si', lein_import),
+        ('sr', lein_require_file),
+        ('ss', lein_send),
     ]
 }
