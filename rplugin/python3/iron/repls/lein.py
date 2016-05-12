3c6a54ef9e3b03e83a8c4e83acb1daba8b8bd435
Henry John Kupty <henry.kupty@nubank.com.br>
2016-05-12 14:31:48 -0300

    Fix plugin call

--- rplugin/python3/iron/repls/lein.py
+++ rplugin/python3/iron/repls/lein.py
@@ -4,22 +4,22 @@
 def lein_require(nvim):
     nvim.command("""normal! "sya]""")
     data = "(require '{})\n".format(nvim.funcs.getreg('s'))
-    return nvim.call('IronSendToRepl', data, "clojure")
+    return nvim.call('IronSend', data, "clojure")
 
 def lein_import(nvim):
     nvim.command("""normal! "syi(""")
     data = "(import '[{}])\n".format(nvim.funcs.getreg('s'))
-    return nvim.call('IronSendToRepl', data, "clojure")
+    return nvim.call('IronSend', data, "clojure")
 
 def lein_require_file(nvim):
     nvim.command("""normal! ggf w"sy$""")
     data = "(require '[{}] :reload)\n".format(nvim.funcs.getreg('s'))
-    return nvim.call('IronSendToRepl', data, "clojure")
+    return nvim.call('IronSend', data, "clojure")
 
 def lein_send(nvim):
     nvim.command("""normal! "sya(""")
     data = "(import '[{}])\n".format(nvim.funcs.getreg('s'))
-    return nvim.call('IronSendToRepl', data, "clojure")
+    return nvim.call('IronSend', data, "clojure")
 
 repl = {
     'command': 'lein repl',
