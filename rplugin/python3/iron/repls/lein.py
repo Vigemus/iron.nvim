e4d997c6dfa878f94bc5c236dde801c2fc02da7d
Henry John Kupty <henry.kupty@nubank.com.br>
2016-05-13 15:21:02 -0300

    Language Mappings

--- rplugin/python3/iron/repls/lein.py
+++ rplugin/python3/iron/repls/lein.py
@@ -3,27 +3,27 @@
 
 def lein_require(nvim):
     nvim.command("""normal! "sya]""")
-    data = "(require '{})\n".format(nvim.funcs.getreg('s'))
+    data = "(require '{})".format(nvim.funcs.getreg('s'))
     return nvim.call('IronSend', data, "clojure")
 
 def lein_import(nvim):
     nvim.command("""normal! "syi(""")
-    data = "(import '[{}])\n".format(nvim.funcs.getreg('s'))
+    data = "(import '[{}])".format(nvim.funcs.getreg('s'))
     return nvim.call('IronSend', data, "clojure")
 
 def lein_require_file(nvim):
     nvim.command("""normal! ggf w"sy$""")
-    data = "(require '[{}] :reload)\n".format(nvim.funcs.getreg('s'))
+    data = "(require '[{}] :reload)".format(nvim.funcs.getreg('s'))
     return nvim.call('IronSend', data, "clojure")
 
 def lein_send(nvim):
     nvim.command("""normal! "sya(""")
-    data = "(import '[{}])\n".format(nvim.funcs.getreg('s'))
+    data = "(import '[{}])".format(nvim.funcs.getreg('s'))
     return nvim.call('IronSend', data, "clojure")
 
 def lein_load_facts(nvim):
     nvim.command("""normal! ggf w"sy$""")
-    data = "(load-facts '{})\n".format(nvim.funcs.getreg('s'))
+    data = "(load-facts '{})".format(nvim.funcs.getreg('s'))
     return nvim.call('IronSend', data, "clojure")
 
 repl = {
--- rplugin/python3/iron/repls/scala.py
+++ rplugin/python3/iron/repls/scala.py
@@ -5,6 +5,28 @@ def sbt_detect(*args, **kwargs):
     import os
     return os.path.exists("build.sbt") or os.path.exists("project/build.sbt")
 
+
+def scala_import_all(nvim):
+    nvim.command("""normal! gg f w"sya]""")
+    data = "import {}._".format(nvim.funcs.getreg('s'))
+    return nvim.call('IronSend', data, "scala")
+
+def scala_import(nvim):
+    nvim.command("""normal! gg f w"sya]""")
+    data = "import {}".format(nvim.funcs.getreg('s'))
+    return nvim.call('IronSend', data, "scala")
+
+def scala_send_block(nvim):
+    nvim.command("""normal! Vi{"sy""")
+    data = "{}".format(nvim.funcs.getreg('s'))
+    return nvim.call('IronSend', data, "scala")
+
+
+mappings = [
+    ('<leader>sa', 'import_all', scala_import_all),
+    ('<leader>si', 'import', scala_import),
+]
+
 sbt = {
     'command': 'sbt console',
     'language': 'scala',
