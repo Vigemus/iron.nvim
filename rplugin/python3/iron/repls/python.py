d217218193907add21b47415d37a29505ace43da
Henry Kupty <hkupty@gmail.com>
2016-05-14 16:28:33 -0300

    Detect python

--- rplugin/python3/iron/repls/python.py
+++ rplugin/python3/iron/repls/python.py
@@ -1,14 +1,19 @@
 # encoding:utf-8
 """Python repl definitions for iron.nvim. """
 
+def detect_ipython(*args, **kwargs):
+    from distutils.spawn import find_executable
+    return find_executable("ipython") is not None
+
 python = {
     'command': 'python',
     'language': 'python',
-    'detect': lambda *args, **kwargs: True,
+    'detect': not detect_ipython,
 }
 
 ipython = {
     'command': 'ipython',
     'language': 'python',
     'multiline': ('%cpaste', '--'),
+    'detect': detect_ipython,
 }
