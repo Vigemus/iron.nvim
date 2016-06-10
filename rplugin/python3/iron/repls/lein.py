844ecb6bd8e0c5db932de88f1d1b6feb7ee1528d
Henry Kupty <hkupty@gmail.com>
2016-06-10 20:12:16 -0300

    Fix clojure commands

--- rplugin/python3/iron/repls/lein.py
+++ rplugin/python3/iron/repls/lein.py
@@ -1,28 +1,33 @@
 # encoding:utf-8
 """Leiningen repl definition for iron.nvim. """
 
+
 def lein_require(nvim):
-    nvim.command("""normal! "sya]""")
+    nvim.command("""normal! mx%"sy%`x""")
     data = "(require '{})".format(nvim.funcs.getreg('s'))
     return nvim.call('IronSend', data, "clojure")
 
+
 def lein_import(nvim):
-    nvim.command("""normal! "syi(""")
+    nvim.command("""normal! mx%"sy%`x""")
     data = "(import '[{}])".format(nvim.funcs.getreg('s'))
     return nvim.call('IronSend', data, "clojure")
 
+
 def lein_require_file(nvim):
-    nvim.command("""normal! ggf w"sy$""")
+    nvim.command("""normal! mxggf w"sy$`x""")
     data = "(require '[{}] :reload)".format(nvim.funcs.getreg('s'))
     return nvim.call('IronSend', data, "clojure")
 
+
 def lein_send(nvim):
-    nvim.command("""normal! "sya(""")
+    nvim.command("""normal! mx?^(<CR>"sya(`x""")
     data = "(import '[{}])".format(nvim.funcs.getreg('s'))
     return nvim.call('IronSend', data, "clojure")
 
+
 def lein_load_facts(nvim):
-    nvim.command("""normal! ggf w"sy$""")
+    nvim.command("""normal! mxggf w"sy$`x""")
     data = "(load-facts '{})".format(nvim.funcs.getreg('s'))
     return nvim.call('IronSend', data, "clojure")
 
