2e063601f7dabbfaa2557474a3ad3cb943eeff4c
Henry Kupty <hkupty@gmail.com>
2016-06-28 22:50:12 -0300

    More code demangling and deduplication

--- rplugin/python3/iron/__init__.py
+++ rplugin/python3/iron/__init__.py
@@ -38,11 +38,26 @@ class Iron(object):
         repl = repl or self.get_current_repl()
         self.__nvim.call('jobsend', repl["repl_id"], data)
 
+    def set_repl_for_ft(self, ft):
+        if ft not in self.__repl:
+            self.__repl[ft] = self.get_repl_template(ft)
+
+        return self.__repl[ft]
+
+    def call_cmd(self, cmd):
+        return self.__nvim.command(cmd)
+
     # Actual Fns
     def open_repl_for(self, ft):
-        self.__nvim.command('spl | wincmd j | enew')
+        repl = self.get_repl_template(ft)
 
-        repl_id = self.termopen(self.__repl[ft]['command'])
+        if not repl:
+            self.__nvim.command("echoerr 'No repl found for {}'".format(ft))
+            return
+
+        self.call_cmd('spl | wincmd j | enew')
+
+        repl_id = self.termopen(repl['command'])
 
         # TODO Make optional nvimux integration detached
         self.__nvim.current.buffer.vars['nvimux_buf_orientation'] = (
@@ -51,11 +66,13 @@ class Iron(object):
 
         base_cmd = 'nnoremap <silent> {} :call IronSendSpecial("{}")<CR>'
 
-        for k, n, c in self.__repl[ft].get('mappings', []):
-            self.__nvim.command(base_cmd.format(k, n))
-            self.__repl['fns'][n] = c
+        for k, n, c in repl.get('mappings', []):
+            self.call_cmd(base_cmd.format(k, n))
+            self.__repl[ft]['fns'][n] = c
 
         self.__repl[ft]['repl_id'] = repl_id
+        self.__nvim.vars["iron_{}_repl".format(ft)] = \
+            self.__nvim.current.buffer.number
 
         return repl_id
 
@@ -73,22 +90,10 @@ class Iron(object):
         self.__nvim.call("inputrestore")
 
         self.open_repl_for(ft)
-        self.__nvim.vars["iron_{}_repl".format(ft)] = \
-            self.__nvim.current.buffer.number
 
     @neovim.command("IronRepl")
     def get_repl(self):
-        ft = self.get_ft()
-
-        repl_type = self.__repl[ft] = self.get_repl_template(ft)
-
-        if not repl_type:
-            self.__nvim.command("echoerr 'No repl found for {}'".format(ft))
-            return
-
-        self.open_repl_for(ft)
-        self.__nvim.vars["iron_{}_repl".format(ft)] = \
-            self.__nvim.current.buffer.number
+        self.open_repl_for(self.get_ft())
 
     @neovim.function("IronSendSpecial")
     def mapping_send(self, args):
@@ -97,9 +102,9 @@ class Iron(object):
     @neovim.function("IronSendMotion")
     def send_motion_to_repl(self, args):
         if args[0] == 'line':
-            self.__nvim.command("""normal! '[V']"sy""")
+            self.call_cmd("""normal! '[V']"sy""")
         else:
-            self.__nvim.command("""normal! `[v`]"sy""")
+            self.call_cmd("""normal! `[v`]"sy""")
 
         return self.send_to_repl([self.__nvim.funcs.getreg('s')])
 
