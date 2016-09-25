006529d31c2e6eb491fa85fcf631904920c65fc1
Henry Kupty <hkupty@gmail.com>
2016-09-25 17:30:58 -0300

    Minor scala improvments

--- rplugin/python3/iron/repls/scala.py
+++ rplugin/python3/iron/repls/scala.py
@@ -2,14 +2,30 @@
 """Scala repl definitions for iron.nvim. """
 from iron.repls.utils.cmd import detect_fn
 
-def scala_import_all(iron):
-    iron.call_cmd("""normal! gg f w"sy$""")
-    data = "import {}._".format(iron.register('s'))
+def get_ns(iron):
+    iron.call_cmd("""normal! mxgg f w"sy$`x""")
+    return iron.register('s')
+
+def get_class(iron):
+    # fcflfa -> Make sure we get to class even if
+    # protected sealed class or protected final class
+    iron.call_cmd("""global/class/normal! 0fcflfab"syt """)
+    return iron.register('s')
+
+def get_object(iron):
+    iron.call_cmd("""global/object/normal! 0fbb"syt """)
+    return iron.register('s')
+
+def scala_import_all_ns(iron):
+    data = "import {}._".format(get_ns(iron))
+    return iron.send_to_repl((data, "scala"))
+
+def scala_import_all_object(iron):
+    data = "import {}.{}._".format(get_ns(iron), get_object(iron))
     return iron.send_to_repl((data, "scala"))
 
 def scala_import(iron):
-    iron.call_cmd("""normal! gg f w"sy$""")
-    data = "import {}".format(iron.register('s'))
+    data = "import {}".format(get_ns(ion))
     return iron.send_to_repl((data, "scala"))
 
 def scala_send_block(iron):
@@ -22,9 +38,13 @@ def scala_send_line(iron):
     data = "{}".format(iron.register('s'))
     return iron.send_to_repl((data, "scala"))
 
+def sbt_test_this_file(iron):
+    data = "test {}.{}".format(get_ns(iron), get_class(iron))
+    return iron.send_to_repl((data, "sbt.scala"))
+
 
 mappings = [
-    ('<leader>sa', 'import_all', scala_import_all),
+    ('<leader>sa', 'import_all', scala_import_all_ns),
     ('<leader>si', 'import', scala_import),
     ('<leader>sb', 'block', scala_send_block),
     ('<leader>sl', 'line', scala_send_line),
@@ -46,7 +66,6 @@ sbt_cmd = {
     'multiline': (':paste\n', '\x04'),
 }
 
-
 scala = {
     'command': 'scala',
     'language': 'scala',
@@ -54,3 +73,8 @@ scala = {
     'mappings': mappings,
     'multiline': (':paste\n', '\x04'),
 }
+
+def do_stuff():
+    nvim.command("2 wincmd w")
+    nvim.command("""global/get_ns/normal! 0f "Gyt(""")
+    return nvim.funcs.getreg("g")
