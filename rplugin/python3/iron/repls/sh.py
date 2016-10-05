a551cab5c7dc01436e0f3daf7b59b1a179e83b8f
Henry Kupty <hkupty@gmail.com>
2016-10-05 18:24:36 -0300

    Add mapping for sh

--- rplugin/python3/iron/repls/sh.py
+++ rplugin/python3/iron/repls/sh.py
@@ -2,10 +2,23 @@
 """Shell 'repl' definition for iron.nvim. """
 from iron.repls.utils.cmd import detect_fn
 
+def prompt_cmd(iron):
+    try:
+        cmd = iron.prompt("command")
+    except:
+        iron.call_cmd("echo 'Aborting'")
+    else:
+        return iron.send_to_repl((cmd, "sh"))
+
+mappings = [
+    ('<leader>sx', 'prompt_cmd', prompt_cmd),
+]
+
 zsh_sh = {
     'command': 'zsh',
     'language': 'sh',
     'detect': detect_fn('zsh'),
+    'mappings': mappings,
 }
 
 zsh_zsh = {
@@ -18,10 +31,12 @@ bash_sh = {
     'command': 'bash',
     'language': 'sh',
     'detect': detect_fn('bash'),
+    'mappings': mappings,
 }
 
 sh_sh = {
     'command': 'sh',
     'language': 'sh',
     'detect': detect_fn('sh'),
+    'mappings': mappings,
 }
