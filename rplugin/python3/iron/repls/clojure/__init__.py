8babac29b32eaefb4517bdf19e80c33a421e0a51
Henry Kupty <hkupty@gmail.com>
2016-11-22 17:09:06 -0200

    connect to local repl if present

--- rplugin/python3/iron/repls/clojure/__init__.py
+++ rplugin/python3/iron/repls/clojure/__init__.py
@@ -4,21 +4,29 @@ from iron.repls.utils.cmd import detect_fn
 from iron.repls.clojure.lein import *
 from iron.repls.clojure.midje import *
 
+mappings = [
+    ('<leader>so', 'require', lein_require),
+    ('<leader>si', 'import', lein_import),
+    ('<leader>sr', 'require-file', lein_require_file),
+    ('<leader>sR', 'require-with-ns', lein_require_with_ns),
+    ('<leader>s.', 'prompt-require', lein_prompt_require),
+    ('<leader>s/', 'prompt-require-as', lein_prompt_require_as),
+    ('<leader>ss', 'send-block', lein_send),
+
+    ('<leader>mf', 'midje-load-facts', midje_load_facts),
+    ('<leader>ma', 'midje-autotest', midje_autotest),
+]
+
+connect = {
+    'command': 'lein repl :connect',
+    'language': 'clojure',
+    'detect': detect_fn('lein', required_files=['.nrepl']),
+    'mappings': mappings
+}
+
 repl = {
     'command': 'lein repl',
     'language': 'clojure',
     'detect': detect_fn('lein'),
-    'nrepl': {'protocol': 'file', 'uri': '.nrepl-port'},
-    'mappings': [
-        ('<leader>so', 'require', lein_require),
-        ('<leader>si', 'import', lein_import),
-        ('<leader>sr', 'require-file', lein_require_file),
-        ('<leader>sR', 'require-with-ns', lein_require_with_ns),
-        ('<leader>s.', 'prompt-require', lein_prompt_require),
-        ('<leader>s/', 'prompt-require-as', lein_prompt_require_as),
-        ('<leader>ss', 'send-block', lein_send),
-
-        ('<leader>mf', 'midje-load-facts', midje_load_facts),
-        ('<leader>ma', 'midje-autotest', midje_autotest),
-    ]
+    'mappings': mappings
 }
