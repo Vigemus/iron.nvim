5b61ce402d209983687a0dc2a3a440f62f0aac31
Aidan <calopter@gmail.com>
2017-02-02 07:07:48 -0500

    wrong dir

--- rplugin/python3/iron/pure.py
+++
@@ -1,9 +0,0 @@
-# encoding:utf-8
-"""Pure-lang repl definitions for iron.nvim. """
-from iron.repls.utils.cmd import detect_fn
-
-repl = {
-    'command': 'pure',
-    'language': 'pure',
-    'detect': detect_fn('pure')
-}
