51d5cc501386786fcd47d5eb5228d9e12ec3a9b0
Henry Kupty <hkupty@gmail.com>
2017-03-30 17:04:59 -0300

    Better get ns using Acid.nvim

--- rplugin/python3/iron/repls/clojure/utils.py
+++ rplugin/python3/iron/repls/clojure/utils.py
@@ -7,6 +7,10 @@ def get_current_parens(iron):
 
 
 def get_current_ns(iron):
+    # Acid is much better at finding the ns than Iron..
+    if iron.nvim.funcs.exists('*AcidGetNs'):
+        return iron.nvim.funcs.AcidGetNs()
+
     iron.call_cmd("""silent normal! mxggf w"sy$`x""")
     return iron.register('s')
 
