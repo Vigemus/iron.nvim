45a0eed023172cf8ca1367f0e71db53edde06bb8
Henry Kupty <hkupty@gmail.com>
2017-11-08 10:27:36 -0200

    Laying ground for sending data to repl

--- spec/iron/core_spec.lua
+++ spec/iron/core_spec.lua
@@ -2,17 +2,20 @@
 -- luacheck: globals before_each after_each
 
 insulate("About #iron functionality", function()
-  before_each(function()
-    _G.vim = mock({ api = {
-      nvim_call_function = function(_, _) return 1 end,
-      nvim_command = function(_) return nil end,
-      nvim_get_option = function(_) return "" end,
-      nvim_get_var = function(_) return "" end,
-    }})
-  end)
-  _G.os = mock({
-      execute = function(_) return 0 end,
-    })
+
+    local fns = require('iron.functional')
+    before_each(function()
+        _G.vim = mock({ api = {
+                    nvim_call_function = function(_, _) return 1 end,
+                    nvim_command = function(_) return nil end,
+                    nvim_get_option = function(_) return "" end,
+                    nvim_get_var = function(_) return "" end,
+            }})
+
+        _G.os = mock({
+                execute = function(_) return 0 end,
+            })
+        end)
 
   after_each(function()
      package.loaded['iron.core'] = nil
@@ -45,6 +48,16 @@ insulate("About #iron functionality", function()
       assert.spy(_G.vim.api.nvim_call_function).was.called_with("exists", {"iron_stuff"})
       assert.spy(_G.vim.api.nvim_get_var).was.called(1)
       assert.spy(_G.vim.api.nvim_get_var).was.called_with("iron_stuff")
+    end)
+  end)
+
+  describe("#memory related", function()
+    it("create_new_repl", function()
+      local iron = require('iron.core')
+      iron.core.create_new_repl("python")
+      assert.are_same(#(fns.keys(iron.memory)), 1)
+      assert.are_not_same(iron.memory.python, nil)
+      assert.are_same(#(fns.keys(iron.memory.python)), 3)
     end)
   end)
 
