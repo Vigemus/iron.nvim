841cdd804b513514118993d66fba6238532ce3a4
Henry Kupty <hkupty@gmail.com>
2017-11-20 18:42:03 -0200

    Restructure ft definition
    So bracketed paste can be reused

--- lua/iron/fts/python.lua
+++ lua/iron/fts/python.lua
@@ -1,18 +1,9 @@
+local common = require('iron.fts.common')
 local py = {}
 
 
-py.ipython = {
-  command = 'ipython',
-  format = function(data)
-      local new = {'\x1b[200~'}
-      for ix, v in ipairs(data) do
-          new[ix+1] = v
-      end
-      new[#new+1] = '\x1b[201~'
-      new[#new+1] = ''
-      return new
-  end
+py.ipython = common.new("bracketed"){
+  command = 'ipython'
 }
-
 
 return py
