e6b6196a1be0350b95247eb7332010918a0ae068
Henry Kupty <hkupty@gmail.com>
2017-11-22 18:32:43 -0200

    Fix spec

--- spec/iron/core_spec.lua
+++ spec/iron/core_spec.lua
@@ -3,11 +3,11 @@
 
 insulate("About #iron functionality", function()
 
-    local _ = require('iron.functional')
+    local _ = require('iron.util.functional')
     before_each(function()
         _G.vim = mock({ api = {
                     nvim_call_function = function(_, _) return 1 end,
-                    nvim_command = function(_) return nil end,
+                    nvim_command = function(_) return "" end,
                     nvim_get_option = function(_) return "" end,
                     nvim_get_var = function(_) return "" end,
             }})
@@ -18,31 +18,32 @@ insulate("About #iron functionality", function()
         end)
 
   after_each(function()
+     package.loaded['iron'] = nil
      package.loaded['iron.core'] = nil
    end)
 
   describe("default #config", function()
     it("doesn't assume preferred values", function()
-      local iron = require('iron.core')
+      local iron = require('iron')
       assert.are_same(iron.config.preferred.python, nil)
     end)
 
     it("has toggle #visibility enabled", function()
-      local iron = require('iron.core')
-      assert.are_same(iron.config.visibility, iron.core.visibility.toggle)
+      local iron = require('iron')
+      assert.are_same(iron.config.visibility, iron.predefs.visibility.toggle)
       end)
   end)
 
   describe("dynamic #config", function()
     it("is not called on a stored config", function()
-      local iron = require('iron.core')
+      local iron = require('iron')
       iron.config.stuff = 1
       local _ = iron.config.stuff
       assert.stub(_G.vim.api.nvim_get_var).was_called(0)
     end)
 
     it("is called on a neovim variable", function()
-      local iron = require('iron.core')
+      local iron = require('iron')
       local _ = iron.config.stuff
       assert.spy(_G.vim.api.nvim_call_function).was_called(1)
       assert.spy(_G.vim.api.nvim_call_function).was.called_with("exists", {"iron_stuff"})
@@ -53,8 +54,8 @@ insulate("About #iron functionality", function()
 
   describe("#memory related", function()
     it("get_repl", function()
-      local iron = require('iron.core')
-      local repl = iron.core.get_repl('python')
+      local iron = require('iron')
+      local repl = iron.core.get_repl(iron.config, iron.memory, 'python')
       assert.are_same(#(_.keys(repl)), 3)
       assert.are_same(#(_.keys(iron.memory)), 1)
       assert.are_not_same(iron.memory.python, nil)
@@ -65,8 +66,8 @@ insulate("About #iron functionality", function()
 
   describe("#core functions", function()
     it("create_new_repl", function()
-      local iron = require('iron.core')
-      iron.core.create_new_repl("python")
+      local iron = require('iron')
+      iron.core.create_new_repl(iron.config, "python")
       assert.spy(_G.os.execute).was_called(1)
       assert.spy(_G.os.execute).was_called_with('which ipython > /dev/null')
       assert.spy(_G.vim.api.nvim_command).was_called(1)
