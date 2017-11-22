b985defb8e8af355e87d9f4f173794c64a066c27
Henry Kupty <hkupty@gmail.com>
2017-11-22 15:52:40 -0200

    Complete  management

--- lua/iron/core.lua
+++ lua/iron/core.lua
@@ -2,21 +2,35 @@
 
 local clone = require("iron.functional").clone
 local visibility = require("iron.visibility")
+local memory_management = require("iron.memory_management")
 
 local defaultconfig = {
   visibility = visibility.toggle,
+  memory_management = memory_management.path_based,
   preferred = {}
 }
 
 local nvim = vim.api
 local iron = {
   memory = {},
+  _ = {},   -- High level functions/function aliases
   core = {
-    visibility = visibility
+    visibility = visibility,
+    memory_management = memory_management
   },
   config = clone(defaultconfig),
   fts = require("iron.fts.fts")
 }
+
+iron.set_config = function(cfg)
+  iron.config = clone(defaultconfig)
+  setmetatable(iron.config, _nvim_proxy)
+
+  for k, v in pairs(cfg) do
+    iron.config[k] = v
+  end
+end
+
 
 local _nvim_proxy = {
   __index = function(_, key)
@@ -59,32 +73,21 @@ iron.core.create_new_repl = function(ft)
   local repl = iron.core.get_preferred_repl(ft)
   local job_id = nvim.nvim_call_function('termopen', {{repl.command}})
   local buffer_id = nvim.nvim_call_function('bufnr', {'%'})
-  iron.memory[ft] = { job = job_id, buffer = buffer_id, definition = repl}
+  return { job = job_id, buffer = buffer_id, definition = repl}
 end
 
-iron.core.get_repl_instance = function(ft)
-  local mem = iron.memory[ft]
-  local newfn = function()
-    iron.core.create_new_repl(ft)
-  end
+iron.core.get_repl = function(ft)
+  local mem = iron.config.memory_management.get(iron.memory, ft)
+  local newfn = function() return iron.core.create_new_repl(ft) end
   local showfn = function()
     nvim.nvim_command(iron.config.repl_open_cmd .. '| b ' .. mem.buffer ..' | set wfw | startinsert')
   end
-
   if mem == nil then
-    newfn()
+    mem = iron.config.memory_management.set(iron.memory, ft, newfn)
   else
     iron.config.visibility(mem.buffer, newfn, showfn)
   end
-end
-
-iron.set_config = function(cfg)
-  iron.config = clone(defaultconfig)
-  setmetatable(iron.config, _nvim_proxy)
-
-  for k, v in pairs(cfg) do
-    iron.config[k] = v
-  end
+  return mem
 end
 
 return iron
