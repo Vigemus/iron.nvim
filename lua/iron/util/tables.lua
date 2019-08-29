465615d205f2e16831569181a7dde6c5af88548a
Henry John Kupty <hkupty@users.noreply.github.com>
2019-08-29 23:55:01 +0200

    Revert single-line wrap issue (#128)
    * Revert "Follow up on #110"
    
    This reverts commit 62755e9645ff4ac78b954af79ff1771aa52f277b.
    
    * Allow format fn to be custom
    
    This resolves the general vs specific issues
    with most language repls and python, so python
    block is now more concise.
    
    * Make scala fn simpler
    
    * Simplify line formatting
    
    Still allows a special case as ipython allows for bracketed pasting
    and that can be done without line breaks.
    
    * Roll back to allow #129 to be merged
    
    * Updated python format (#129)
    
    Append close marker to the end of last line instead of creating
    new empty line.
    
    * Fix tests

--- lua/iron/util/tables.lua
+++ lua/iron/util/tables.lua
@@ -63,4 +63,26 @@ fns.merge = function (...)
   return res
 end
 
+fns.extend = function(...)
+  local tbl = {}
+  local tbls = {n = select("#", ...), ...}
+  for ix=1, tbls.n do
+    local itm = tbls[ix]
+    if itm ~= nil then
+
+      if type(itm) == "table" then
+        for _, i in ipairs(itm) do
+          table.insert(tbl, i)
+        end
+      else
+        table.insert(tbl, itm)
+      end
+
+    end
+  end
+
+  return tbl
+end
+
+
 return fns
