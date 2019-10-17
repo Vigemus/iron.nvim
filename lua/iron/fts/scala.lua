db410f58ec421caaa0885f4f0280ecfb3b7112d2
Henry John Kupty <hkupty@users.noreply.github.com>
2019-10-17 23:48:38 +0200

    Fix #110: Use generic instead of custom function (#133)
    This now should properly work

--- lua/iron/fts/scala.lua
+++ lua/iron/fts/scala.lua
@@ -4,13 +4,8 @@ local scala = {}
 local def = function(command)
   return {
     command = command,
-    format = function(lines)
-      if #lines == 1 then
-        return {lines[1]}
-      end
-
-      return extend(":paste", lines, "\04")
-    end
+    open = ":paste",
+    close = "\04"
   }
 end
 
