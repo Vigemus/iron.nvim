8d02a5dfe9d5fbdf6aca22f29a843ec0b05d2c31
Henry Kupty <hkupty@gmail.com>
2021-01-05 22:15:52 +0100

    split init into lowlevel and core
    Also, moves extmark related to own ns;
    This allows for more concise modules.
    
    Additionally, fixes off-by-one in extmark caused by columns being -1.
    
    Moreso, creates the highlight 'IronLastSent', which turns the last
    chunk that was sent to bold

---
+++ lua/iron/defaults.lua
@@ -0,0 +1,29 @@
+local view = require("iron.view")
+local tables = require("iron.util.tables")
+local namespace = vim.api.nvim_create_namespace("iron")
+
+local defaults = {
+  highlight_last = "IronLastSent",
+  visibility = require("iron.visibility").toggle,
+  scope = require("iron.scope").path_based,
+  preferred = {},
+  repl_open_cmd = view.openwin('topleft vertical 100 split'),
+  namespace = namespace,
+  mark = { -- Arbitrary numbers
+    save_pos = 20,
+    send = 77,
+    begin_last = 99, -- Deprecated
+    end_last = 100 -- Deprecated
+  },
+}
+
+return setmetatable({
+    _defaults = function() return tables.clone(defaults) end
+  }, {
+  __newindex = function(tbl, k v)
+    error("Don't alter default table. Change iron.config instead", 2)
+  end,
+  __index = function(_, k)
+    return defaults[k]
+  end
+})
