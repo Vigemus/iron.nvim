ce4a3157a4fd6e5d0bdc970e69a183bccb611ccd
f3fora <33097027+f3fora@users.noreply.github.com>
2021-05-09 10:37:24 +0200

    cpp.root: code more linear, add regex condition

--- lua/iron/fts/cpp.lua
+++ lua/iron/fts/cpp.lua
@@ -10,38 +10,37 @@ local function magic(str)
     -- remove every space at end of a line
     str = str:gsub('%s+$', '')
     -- remove line break and extra spaces eventually
-    str = str:gsub('\\$', '')
+    str = str:gsub([[\$]], '')
     str = str:gsub('%s+$', '')
     return str
 end
 
 local function lastmagic(str)
     -- check what character is the last of the line
+    if str:len() == 0 then return false end
     local lastchar = str:sub(-1, -1)
     return lastchar == ';' or lastchar == '{' or lastchar == '}'
 end
 
-local format = function(cr)
-    return function(lines)
-        if #lines == 1 then
-            return {magic(lines[1]) .. cr}
-        else
-            local new = {}
-            local aus = ''
-            for line = 1, #lines do
-                -- concatenate lines if they do not end with a lastmagic character.
-                local l = magic(lines[line])
-                aus = aus .. l
-                if lastmagic(l) then
-                    table.insert(new, aus)
-                    aus = ''
-                end
+local function format(lines)
+    if #lines == 1 then
+        return {magic(lines[1]) .. '\13'}
+    else
+        local new = {}
+        local aus = ''
+        for line = 1, #lines do
+            -- concatenate lines if they do not end with a lastmagic character.
+            local l = magic(lines[line])
+            aus = aus .. l
+            if lastmagic(l) then
+                table.insert(new, aus)
+                aus = ''
             end
-            return extend(new, cr)
         end
+        return extend(new, '')
     end
 end
 
-cpp.root = {command = {'root', '-l'}, format = format('\13')}
+cpp.root = {command = {'root', '-l'}, format = format}
 
 return cpp
