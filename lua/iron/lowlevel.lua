be164501fa85157f023d6aa10014d06ebfdb1616
Henry Kupty <hkupty@gmail.com>
2022-01-19 22:37:56 +0100

    Move deprecated functions down
    So they are easier to find.
    Also, adds documentation to the functions that are being kept.

--- lua/iron/lowlevel.lua
+++ lua/iron/lowlevel.lua
@@ -25,6 +25,115 @@ ll.get_buffer_ft = function(bufnr)
   end
 end
 
+--- Creates the repl in the current window
+-- This function effectively creates the repl without caring
+-- about window management. It is expected that the client
+-- ensures the right window is created and active before calling this function
+-- @param repl definition of the repl being created
+-- @param repl.command table with the command to be invoked.
+ll.create_repl_on_current_window = function(repl)
+  local bufnr = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_win_set_buf(winid, bufnr)
+  local job_id = vim.fn.termopen(repl.command)
+
+  return {
+    bufnr = bufnr,
+    job = job_id,
+    repldef = repl,
+    winid = vim.api.nvim_get_current_win()
+  }
+
+end
+
+--- Wrapper function for getting repl definition from config
+-- This allows for an easier transition between old and new methods
+ll.get_repl_def = function(ft)
+  local repl = config.repl_definitions[ft]
+  if repl == nil then
+    -- TODO Remove after deprecated fns are cleaned
+    return ll.get_preferred_repl(ft)
+  end
+  return repl
+end
+
+--- Creates a new window for placing a repl.
+-- Expected to be called before creating the repl.
+-- It knows nothing about the repl and only takes in account the
+-- configuration.
+ll.new_window = function()
+  -- Buffer is created because old view APIs required.
+  -- Ideally, they create scratch buffers until the actual
+  -- buffer is placed on the window, but since this is missing,
+  -- we provide a scratch buffer ourselves here
+  local bufnr = vim.api.nvim_create_buf(false, true)
+  if type(config.repl_open_cmd) == "function" then
+    return config.repl_open_cmd(buff)
+  else
+    return view.openwin(config.repl_open_cmd, buff)
+  end
+end
+
+--- Conditional execution depending on repl existence
+-- This fn wraps the logic of doing something if a repl exists or not.
+-- Since this pattern repeats frequently, this is a way of wrapping the complexity
+-- and skipping the need to "ensure a repl exists", for example.
+ll.if_repl_exists = function(ft, when_true_action, when_false_action)
+  local mem = ll.get(ft)
+
+  if mem ~= nil and when_true_action ~= nil then
+    return when_true_action(mem), true
+  elseif when_false_action ~= nil then
+    return when_false_action(), false
+  end
+
+  return nil, nil
+end
+
+ll.send_to_repl = function(ft, data)
+  local dt = data
+
+  if type(data) == "string" then
+    dt = vim.split(data, '\n')
+  end
+
+  local mem = ll.get(ft)
+
+  dt = format(mem.repldef, dt)
+
+  local window = vim.fn.win_getid(vim.fn.bufwinnr(mem.bufnr))
+  vim.api.nvim_win_set_cursor(window, {vim.api.nvim_buf_line_count(mem.bufnr), 0})
+
+  local indent = ""
+  for i, v in ipairs(dt) do
+    if #v == 0 then
+      dt[i] = indent
+    else
+      indent = string.match(v, '^(%s)') or ""
+    end
+  end
+  vim.api.nvim_call_function('chansend', {mem.job, dt})
+end
+
+ll.get_repl_ft_for_bufnr = function(bufnr)
+  -- given a buffer number, tries to look up the corresponding
+  -- filetype of the REPL
+  -- If the corresponding buffer number does not exist or is not
+  -- a REPL, then return nil
+  local ft_found = nil
+  for ft in pairs(ll.store) do
+    local mem = ll.get(ft)
+    if mem ~= nil and bufnr == mem.bufnr then
+      ft_found = ft
+      break
+    end
+  end
+  return ft_found
+end
+
+
+
+-- [[ Below this line are deprecated functions to be removed ]] --
+
 -- Deprecated
 ll.get_preferred_repl = function(ft)
   local repl_definitions = fts[ft]
@@ -49,6 +158,7 @@ ll.get_preferred_repl = function(ft)
   return repl_def
 end
 
+-- Deprecated
 ll.new_repl_window = function(buff, ft)
   if type(config.repl_open_cmd) == "function" then
     return config.repl_open_cmd(buff, ft)
@@ -57,26 +167,6 @@ ll.new_repl_window = function(buff, ft)
   end
 end
 
-
---- Creates the repl in the current window
--- This function effectively creates the repl without caring
--- about window management. It is expected that the client
--- ensures the right window is created and active before calling this function
--- @param repl definition of the repl being created
--- @param repl.command table with the command to be invoked.
-ll.create_repl_on_current_window = function(repl)
-  local bufnr = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_win_set_buf(winid, bufnr)
-  local job_id = vim.fn.termopen(repl.command)
-
-  return {
-    bufnr = bufnr,
-    job = job_id,
-    repldef = repl,
-    winid = vim.api.nvim_get_current_win()
-  }
-
-end
 
 -- Deprecated
 ll.create_new_repl = function(ft, repl, new_win)
@@ -129,6 +219,7 @@ ll.create_preferred_repl = function(ft, new_win)
     return nil
 end
 
+-- Deprecated
 ll.ensure_repl_exists = function(ft)
   local mem = ll.get(ft)
   local created = false
@@ -139,46 +230,6 @@ ll.ensure_repl_exists = function(ft)
   end
 
   return mem, created
-end
-
-ll.send_to_repl = function(ft, data)
-  local dt = data
-
-  if type(data) == "string" then
-    dt = vim.split(data, '\n')
-  end
-
-  local mem = ll.get(ft)
-
-  dt = format(mem.repldef, dt)
-
-  local window = vim.fn.win_getid(vim.fn.bufwinnr(mem.bufnr))
-  vim.api.nvim_win_set_cursor(window, {vim.api.nvim_buf_line_count(mem.bufnr), 0})
-
-  local indent = ""
-  for i, v in ipairs(dt) do
-    if #v == 0 then
-      dt[i] = indent
-    else
-      indent = string.match(v, '^(%s)') or ""
-    end
-  end
-  vim.api.nvim_call_function('chansend', {mem.job, dt})
-end
-
-ll.get_repl_ft_for_bufnr = function(bufnr)
-  -- given a buffer number, tries to look up the corresponding
-  -- filetype of the REPL
-  -- If the corresponding buffer number does not exist or is not
-  -- a REPL, then return nil
-  local ft_found = nil
-  for ft in pairs(ll.store) do
-    local mem = ll.get(ft)
-    if mem ~= nil and bufnr == mem.bufnr then
-      ft_found = ft
-    end
-  end
-  return ft_found
 end
 
 
