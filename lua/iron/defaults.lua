b9fe12c77f12b002864640f21ece6870a3ba1a43
Henry Kupty <hkupty@gmail.com>
2022-01-19 22:03:20 +0100

    Prepare simplification of repl definition
    It seems convoluted that the users have to:
      a) create a repl definition to an arbitrary name;
      b) refer to that arbitrary name to get the definition;
    
    This seems to cause a lot of confusion.
    The new structure should allow for a direct reference:
    
    {
      repl_definition = {
        python = {
          command = {"ipython3", "--whatever-flags"}
        }
      }
    }
    
    So the caller code will look up in the config and, if no
    value is explicitly defined, it will lookup with the metamethod
    to get the first available repl definition.
    
    Explicitly defining the preferred repl definition
    would be as easy as:
    
    {
      repl_definition = {
        python = require("iron.fts.python").ipython
      }
    }

--- lua/iron/defaults.lua
+++ lua/iron/defaults.lua
@@ -6,14 +6,36 @@ local defaults = {
   highlight_last = "IronLastSent",
   visibility = require("iron.visibility").toggle,
   scope = require("iron.scope").path_based,
-  preferred = {},
+  preferred = setmetatable({}, {
+    __newindex = function(tbl, k, v)
+      vim.api.nvim_err_writeln("iron: Setting preferred repl is deprecated.")
+      vim.api.nvim_err_writeln("      Use `repl_definition` key instead supplying the complete repl definition")
+      rawset(tbl, k, b)
+    end
+  }),
+  repl_definition = setmetatable({}, {
+    __index = function(tbl, key)
+      local repl_definitions = require("iron.fts")[key]
+      local repl_def
+      for _, v in pairs(repl_definitions) do
+        if vim.fn.executable(v.command[1]) == 1 then
+          repl_def = v
+          break
+        end
+      end
+      if repl_def == nil then
+        vim.api.nvim_err_writeln("Failed to locate REPL executable, aborting")
+      else
+        rawset(tbl, key, repl_def)
+        return repl_def
+      end
+    end
+  }),
   repl_open_cmd = view.openwin('topleft vertical 100 split'),
   namespace = namespace,
   mark = { -- Arbitrary numbers
     save_pos = 20,
     send = 77,
-    begin_last = 99, -- Deprecated
-    end_last = 100 -- Deprecated
   },
   buflisted = false,
 }
