41b31f3337e20348fcf841de068763b7a6ba7758
Henry Kupty <hkupty@gmail.com>
2022-03-15 10:09:41 +0100

    Remove unnecessary comments from init

--- lua/iron/init.lua
+++ lua/iron/init.lua
@@ -1,18 +1,5 @@
 -- luacheck: globals unpack vim
 
--- [[ -> iron
--- Here is the complete iron API.
--- Below is a brief description of module separation:
---  -->> ll:
---    Low level functions that interact with neovim's windows and buffers.
---
---  -->> config:
---    This is what guides irons behavior.
---
---  -->> core:
---    User api, should have all public functions there.
--- ]]
-
 local iron = {
   -- Will eventually be removed
   config = require("iron.config"),
