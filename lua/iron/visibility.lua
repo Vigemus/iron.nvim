00d21e5e4c963895e987775d075010dcbc9b3bc0
Henry Kupty <hkupty@gmail.com>
2022-03-21 22:30:52 +0100

    Rename function + Stop focusing on toggle
    To keep toggle consistent, we should not focus on the repl when
    returning. It can be done by `IronFocus` anyway, which is a keymap away
    from the user.

--- lua/iron/visibility.lua
+++ lua/iron/visibility.lua
@@ -1,7 +1,12 @@
 -- luacheck: globals unpack vim
 local visibility = {}
 
-local hidden = function(bufid, showfn)
+--- Show hidden window
+-- Creates a window for a buffer if it was previously
+-- hidden by nvim_win_hide, otherwise does nothing.
+-- @return window handle
+-- @treturn boolean wither the widnow was hidden or not
+local show_hidden = function(bufid, showfn)
   local was_hidden = false
   local window = vim.fn.bufwinid(bufid)
 
@@ -14,21 +19,19 @@ local hidden = function(bufid, showfn)
 end
 
 visibility.single = function(bufid, showfn)
-  hidden(bufid, showfn)
+  show_hidden(bufid, showfn)
 end
 
 visibility.toggle = function(bufid, showfn)
-  local window, was_hidden = hidden(bufid, showfn)
+  local window, was_hidden = show_hidden(bufid, showfn)
   if not was_hidden then
     vim.api.nvim_win_hide(window)
-  else
-    vim.api.nvim_set_current_win(window)
   end
 end
 
 visibility.focus = function(bufid, showfn)
-  local window = hidden(bufid, showfn)
-    vim.api.nvim_set_current_win(window)
+  local window = show_hidden(bufid, showfn)
+  vim.api.nvim_set_current_win(window)
 end
 
 return visibility
