c6c293ade504bb149f4de1a981d9fb59e7db09f3
Henry John Kupty <hkupty@users.noreply.github.com>
2022-06-01 09:58:57 +0200

    Cleanup R definitions (#257)
    Remove duplicated code

--- lua/iron/fts/r.lua
+++ lua/iron/fts/r.lua
@@ -1,28 +1,13 @@
+local bracketed_paste = require("iron.fts.common").bracketed_paste
 local r = {}
 
 r.R = {
     command = { "R" },
 }
 
-local extend = require("iron.util.tables").extend
-
-local format = function(open, close, cr)
-    return function(lines)
-        if #lines == 1 then
-            return { lines[1] .. cr }
-        else
-            local new = { open .. lines[1] }
-            for line = 2, #lines do
-                table.insert(new, lines[line])
-            end
-            return extend(new, close)
-        end
-    end
-end
-
 r.radian = {
     command = { "radian" },
-    format = format("\27[200~", "\27[201~\13", "\13"),
+    format = bracketed_paste
 }
 
 return r
