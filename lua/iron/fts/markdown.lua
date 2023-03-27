b1ecaf92cde22d1c65e11167170d6c37885d0489
milanglacier <dev@milanglacier.com>
2023-03-27 11:48:41 -0400

    fix(aichat): replace tab by spaces to avoid invoking completion. (#325)

--- lua/iron/fts/markdown.lua
+++ lua/iron/fts/markdown.lua
@@ -1,17 +1,25 @@
 local markdown = {}
 
+local function replace_tab_by_spaces(line)
+  local spaces = string.rep(" ", 8)
+  return string.gsub(line, "\t", spaces)
+end
+
 local aichat_format = function(lines)
   local cr = "\13"
   local open_code = "{"
   local close_code = "}"
   -- aichat currently use `{` and `}`
   -- to determine whether it is multi line input.
+  -- aichat uses tab for invoking completion,
+  -- there's no way to insert a real tab,
+  -- we can only replace tab by spaces.
   if #lines == 1 then
-    return { lines[1] .. cr }
+    return { replace_tab_by_spaces(lines[1]) .. cr }
   else
-    local new = { open_code .. lines[1] .. cr }
+    local new = { open_code .. replace_tab_by_spaces(lines[1]) .. cr }
     for line = 2, #lines do
-      table.insert(new, lines[line] .. cr)
+      table.insert(new, replace_tab_by_spaces(lines[line]) .. cr)
     end
 
     table.insert(new, close_code .. cr)
