3220cb3a66dd2ac077c03f30f5fafeb9efe68455
Jorge Diaz <12342395-jorgeldb@users.noreply.gitlab.com>
2024-04-27 10:02:49 -0500

    fix: indentation problem in windows
    remove windows check and windows-specific linefeed since
    bracketed_paste_python works on windows and fixes indentation problem

--- lua/iron/fts/python.lua
+++ lua/iron/fts/python.lua
@@ -3,22 +3,10 @@ local bracketed_paste = require("iron.fts.common").bracketed_paste
 local bracketed_paste_python = require("iron.fts.common").bracketed_paste_python
 local python = {}
 
-local has = function(feature)
-  return vim.api.nvim_call_function('has', {feature}) == 1
-end
-
 local executable = function(exe)
   return vim.api.nvim_call_function('executable', {exe}) == 1
 end
 
-local windows_linefeed = function(lines)
-  for idx,line in ipairs(lines) do
-    lines[idx] = line .. '\13'
-  end
-  return lines
-end
-
-local is_windows = has('win32') and true or false
 local pyversion  = executable('python3') and 'python3' or 'python'
 
 local def = function(cmd)
@@ -36,9 +24,5 @@ python.python = {
   format = bracketed_paste_python,
   close = {""}
 }
-
-if is_windows then
-  python.python.format = windows_linefeed
-end
 
 return python
