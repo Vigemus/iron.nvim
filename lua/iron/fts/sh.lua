88cd340407b959f1168af2a6ae5a3d180e6df32c
Grafcube <grafcube@disroot.org>
2026-02-21 05:24:30 +0530

    Update shell ft checking (#447)

--- lua/iron/fts/sh.lua
+++ lua/iron/fts/sh.lua
@@ -1,15 +1,22 @@
 local sh = {}
 
-sh.zsh = {
-  command = {"zsh"},
-}
-
 sh.bash = {
   command = {"bash"},
 }
 
 sh.sh = {
-  command = {"sh"},
+  command = function(meta)
+    local bufnr = meta.current_bufnr
+    if vim.b[bufnr].is_posix == 1 then
+      return {"sh"}
+    elseif vim.b[bufnr].is_bash == 1 then
+      return {"bash"}
+    elseif vim.b[bufnr].is_kornshell == 1 then
+      return {"ksh"}
+    else
+      return {"sh"}
+    end
+  end,
 }
 
 return sh
